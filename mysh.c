61fe848fe40bce93624f220c1c8a513340c1b18f
rahulcb03 <125521382+rahulcb03@users.noreply.github.com>
2023-03-17 19:09:56 -0400

    Add files via upload

--- mysh.c
+++ mysh.c
@@ -7,47 +7,60 @@
 #define BUFSIZE 100
 
 int main(int argc , char** argv) {
-	if(argc ==1 ){
-		//run in interactive mode 
-
-
-	}else{
+	int fd; 
+	if(argc == 1){
+		fd =0; 
+		char message[] = {"Welcome to my Shell!\nmysh> "}; 
+		if(write(1, message, strlen(message) ) == -1 ){ 
+			perror("Error writing to STDOUT");
+			return EXIT_FAILURE; 
+		}
+	}
+	else{
 		if( argc==2){
 			//run in Batch mode
-		
+			
 			//open the file given and check if it exists
-			int fd = open(argv[1],O_RDONLY); 
+			fd = open(argv[1],O_RDONLY); 
 			if(fd==-1){
-				perror("failed to open file"); 
+				perror("failed to open file"); 	
 				return EXIT_FAILURE; 
 			}
+		}
+		else{ return EXIT_FAILURE; }
+
+	}
+		
+		char* token; 
+		char str[] = {"mysh> "}; 	
+		char buffer[BUFSIZE]; 
+		ssize_t bytes = read(fd, buffer, BUFSIZE-1); 
+		
+		//read in the bytes until end of file
+		while(bytes != 0) {
 			
-			char* token; 
-			char buffer[BUFSIZE]; 
-			ssize_t bytes = read(fd, buffer, BUFSIZE-1); 
-			
-			//read in the bytes until end of file
-			while(bytes != 0) {
+			buffer[bytes] = '\0'; 
+			token = strtok(buffer, "\n"); 
+
+			while(token != NULL ){
+				//do something with the token
 				
-				buffer[bytes] = '\0'; 
-				token = strtok(buffer, "\n"); 
-
-				while(token != NULL ){
-					//do something with the token
-					
-					token = strtok(NULL, "\n"); 
-				}
-
-				// do something when buffer does not end with new line
-				
-				bytes = read(fd , buffer, BUFSIZE -1); 
-
-
+				token = strtok(NULL, "\n"); 
 			}
+
+			// do something when buffer does not end with new line
 			
+			if(argc ==1){
+				if(write(1, str, strlen(str) ) == -1 ){
+					perror("Error reading to STDOUT"); 
+					return EXIT_FAILURE; 
+				}
+			}
+			bytes = read(fd , buffer, BUFSIZE -1); 
+
 
 		}
-		else{return EXIT_FAILURE; }
-	}
+				
+	
 	return EXIT_SUCCESS; 
 }
